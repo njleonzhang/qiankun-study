857853eae732d2b2e683e5719f0fa5642a12056d
njleonzhang <scream3616@sina.com>
2021-03-29 20:33:29 +0800

    fix

--- master/src/pages/home/list.jsx
+++ master/src/pages/home/list.jsx
@@ -4,8 +4,7 @@ function list(props) {
   return (
     <div>
       I am user list, I am in master project
-      {/* <MicroApp name='list' base="home"></MicroApp> */}
-      <MicroApp name='list'></MicroApp>
+      <MicroApp name='list' base="/home/list"></MicroApp>
     </div>
   )
 }
